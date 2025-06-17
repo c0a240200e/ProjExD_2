97a225649f1d27196ef5513ffe8cc0b80923b354
M.Yoda <c0a240200e@edu.teu.ac.jp>
2025-06-17 17:31:00 +0900

    ex2&ex3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,7 @@ DELTA = {
     pg.K_LEFT:(-5,0),  
     pg.K_RIGHT:(5,0),
 }
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 def check_bound(rct: pg.Rect) -> tuple[bool,bool]:
     """
@@ -41,8 +42,8 @@ def main():
     bb_rct = bb_img.get_rect()
     bb_rct.centerx = random.randint(0,WIDTH)
     bb_rct.centery =random.randint(0,HEIGHT)
-    vx = +5#爆弾の移動速度
-    vy = +5
+    vx = 5#爆弾の移動速度
+    vy = 5
     gg_img = pg.Surface((WIDTH, HEIGHT))
     pg.draw.rect(gg_img, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
     gg_img.set_alpha(50) 
@@ -66,6 +67,41 @@ def main():
         screen.blit(text, text_rect)
         pg.display.update()
         time.sleep(5)
+        """
+        ：ゲームオーバー時に，半透明の黒い画面上に「Game Over」と表
+        示し，泣いているこうかとん画像を貼り付ける関数
+        """
+    
+    def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+        bb_imgs = []# 爆弾の画像リスト
+        bb_accs = []# 爆弾の加速度リスト
+        for r in range(1,11):
+            bb_img = pg.Surface((20*r,20*r))# 爆弾の大きさを変える
+            pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+            bb_img.set_colorkey((0,0,0))
+            bb_imgs.append(bb_img)
+            bb_accs.append(r) # 爆弾の加速度をリストに追加
+        return bb_imgs, bb_accs
+    kk_img_dict = {
+        """こうかとんの飛ぶ方向による向きの変換実装途中"""
+        (0, -5): pg.transform.rotozoom(kk_img, 0, 0.9),     # 上
+        (5, 0): pg.transform.flip(pg.transform.rotozoom(kk_img, -90, 0.9), True, False),  # 右（反転）
+        (0, 5): pg.transform.rotozoom(kk_img, 180, 0.9),     # 下
+        (-5, 0): pg.transform.rotozoom(kk_img, 90, 0.9),     # 左
+        (-5, -5): pg.transform.rotozoom(kk_img, 45, 0.9),    # 左上
+        (5, -5): pg.transform.flip(pg.transform.rotozoom(kk_img, -45, 0.9),True,False),    # 右上
+        (-5, 5): pg.transform.rotozoom(kk_img, 135, 0.9),    # 左下
+        (5, 5): pg.transform.rotozoom(kk_img, -135, 0.9),    # 右下
+        (0, 0): pg.transform.rotozoom(kk_img, 0, 0.9)        # 静止
+    }
+
+    def get_kk_img(sum_mv:tuple[int,int]) -> pg.Surface:
+        """
+        移動量タプルに対応する画像Surfaceを返す関数
+        """
+        return kk_img_dict.get(sum_mv, kk_img_dict[(0, 0)])
+    
+
 
     while True:
         for event in pg.event.get():
@@ -82,18 +118,26 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        kk_img = get_kk_img((0, 0))
+        kk_img = get_kk_img(tuple(sum_mv))
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True,True):
             kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
-        bb_rct.move_ip(vx,vy)
+        
+        screen.blit(kk_img, kk_rct)
+        bb_imgs,bb_accs = init_bb_imgs()
+        avx = vx*bb_accs[min(tmr//500,9)]
+        avy = vy*bb_accs[min(tmr//500,9)]
+        bb_img = bb_imgs[min(tmr//500,9)]
+        bb_rct.move_ip(avx,avy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:#横方向にはみ出たら
             vx *= -1
         if not tate:
             vy *= -1
-
-        screen.blit(kk_img, kk_rct)
+        
         screen.blit(bb_img,bb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
