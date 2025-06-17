cf2a60d24a1be0b2f75c39048838602986b4fdaa
M.Yoda <c0a240200e@edu.teu.ac.jp>
2025-06-17 14:15:42 +0900

    P2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -18,10 +19,19 @@ def main():
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
+    bb_img = pg.Surface((20,20))#からのsurfaceを作成
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0,0,0))
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_rct = bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery =random.randint(0,HEIGHT)
+    vx = +5#爆弾の移動速度
+    vy = +5
     clock = pg.time.Clock()
     tmr = 0
+    
 
     while True:
         for event in pg.event.get():
@@ -44,7 +54,9 @@ def main():
         # if key_lst[pg.K_RIGHT]:
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
+        bb_rct.move_ip(vx,vy)
         screen.blit(kk_img, kk_rct)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
